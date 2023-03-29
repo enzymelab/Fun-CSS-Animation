3376db301e7fa5e7d5430b367398288fd88b8999
enzymelab <enzymelab@yahoo.com>
2023-03-29 11:23:21 -0500

    running

---
+++ runningline-react/running/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import BackgroundAnimation from './BackgroundAnimation';
+function App() {
+  return (
+    <div className="App">
+      
+      <BackgroundAnimation/>
+      
+    </div>
+  );
+}
+
+export default App;
